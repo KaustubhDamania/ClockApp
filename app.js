a6cf1c3b04d7e8b0e04608731ba9f4791fcfdb29
KaustubhDamania <kaustubh.damania@gmail.com>
2019-05-10 02:06:52 +0530

    Misc changes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 function draw(ctx, centreX, centreY, radius, startAngle, endAngle, colorOfStroke) {
   ctx.beginPath();
   ctx.arc(centreX,centreY,radius,startAngle,endAngle,false);
-  ctx.lineWidth=20;
+  ctx.lineWidth=50;
   ctx.strokeStyle=colorOfStroke;
   ctx.stroke();
 }
@@ -21,37 +21,44 @@ window.onload = function() {
   hr=d.getHours()%12;
   min=d.getMinutes();
   sec=d.getSeconds();
-  //console.log(hr+'.'+min+'.'+sec);
+
+  hrRadius = 175;
+  minRadius = 225;
+  secRadius = 275;
+  dotRadius = 350;
+  outerRadius = 400;
+  whiteColor = '#F0F0F0';
+
   var hrClock, minClock, secClock;
   hrClock=hr*pi/6//+(pi/360*sec);
   minClock=min*pi/30;
   secClock=sec*pi/30;
   var c=document.getElementById("canvas2");
   var ctx=c.getContext("2d");
-  draw(ctx,centerCanvasX,centerCanvasY,300,3*pi/2,secClock-pi/2,'#FF6A6A');
+  draw(ctx,centerCanvasX,centerCanvasY,secRadius,3*pi/2,secClock-pi/2,'#FF6A6A');
   if(sec==0 || sec==0 && min==0)
   {
     ctx.clearRect(0,0,canvas2.width,canvas2.height);
   }
-  draw(ctx,centerCanvasX,centerCanvasY,250,3*pi/2,minClock-pi/2,'#00BFA5');
-  draw(ctx,centerCanvasX,centerCanvasY,200,3*pi/2,hrClock-pi/2,'#3D5AFE');
+  draw(ctx,centerCanvasX,centerCanvasY,minRadius,3*pi/2,minClock-pi/2,'#00BFA5');
+  draw(ctx,centerCanvasX,centerCanvasY,hrRadius,3*pi/2,hrClock-pi/2,'#3D5AFE');
   ctx.beginPath();
-  ctx.arc(centerCanvasX,centerCanvasY,400,0,2*pi,false);
+  ctx.arc(centerCanvasX,centerCanvasY,outerRadius,0,2*pi,false);
   ctx.lineWidth=5;
-  ctx.strokeStyle="black";
+  ctx.strokeStyle="white";
   ctx.stroke();
   //draw(ctx,centerCanvasX,centerCanvasY,350,0,2*pi,'#000000');
   //writeText(ctx,centerCanvasX,centerCanvasY,"Hello","red");
   for(var i=1; i<=60; i++)
   {
-    var posX = centerCanvasX - (350*Math.sin((-2*i*pi/60)));
-    var posY = centerCanvasY - (350*Math.cos((-2*i*pi/60)));
+    var posX = centerCanvasX - (dotRadius*Math.sin((-2*i*pi/60)));
+    var posY = centerCanvasY - (dotRadius*Math.cos((-2*i*pi/60)));
     if(i%5==0)
     {
-      writeText(ctx,posX,posY,i/5,"black");
+      writeText(ctx,posX,posY,i/5,whiteColor);
       continue;
     }
-    writeText(ctx,posX,posY,".","black");
+    writeText(ctx,posX,posY,".",whiteColor);
   }
 }
 setInterval(window.onload, 1000);
